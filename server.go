a812f7c95ca288d12003e78621c904854c1e0691
felixwatts <felixwatts@gmail.com>
2019-01-20 14:24:51 +0100

    get webhook parsing working

--- server.go
+++ server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 	"strconv"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -12,20 +15,43 @@ import (
 
 const PortHttp = 8081
 const PortHttps = 8443
-const HttpsRedirectRoot = "https://etherdirect.co.uk:443"
+const HttpsRedirectRoot = "https://localhost:8443"
 const HttpsCertificate = "/etc/letsencrypt/live/etherdirect.co.uk/fullchain.pem"
 const HttpsPrivateKey = "/etc/letsencrypt/live/etherdirect.co.uk/privkey.pem"
-const FileSystemRoot = "/var/www/etherdirect.co.uk/"
+const FileSystemRoot = "./"
 const AddressEtherDirect = "0xDaEF995931D6F00F56226b29ba70353327b21E00"
-const MinOrderAmountPence = 500
-const MaxOrderAmountPence = 500
+const OrderAmountPence = 500
 const ServiceChargePence = 100
+const EtherValuePence = OrderAmountPence - ServiceChargePence
 
-type MonzoTransaction struct {
+type MonzoWebHookCounterParty struct {
+	Name           string
+	Sort_Code      string
+	Account_Number string
+}
+
+type MonzoWebHookTransaction struct {
+	Description  string
+	Amount       uint
+	Currency     string
+	CounterParty MonzoWebHookCounterParty
+}
+
+type MonzoWebHook struct {
+	Type string
+	Data MonzoWebHookTransaction
+}
+
+type Order struct {
 	SortCode      string
 	AccountNumber string
-	AmountPence   uint
+	Currency      string
+	Amount        uint
 	EthAddress    common.Address
+}
+
+func (o Order) String() string {
+	return fmt.Sprintf("{ %s %s %s %d %s }", o.SortCode, o.AccountNumber, o.Currency, o.Amount, o.EthAddress.Hex())
 }
 
 type IndexViewModel struct {
@@ -33,54 +59,137 @@ type IndexViewModel struct {
 
 var templates = make(map[string]*template.Template)
 
-func ParseMonzoTx(r *http.Request) (err error, tx MonzoTransaction) {
-	return errors.New("Not implemented"), MonzoTransaction{}
+func IsValidAddress(v string) bool {
+	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+	return re.MatchString(v)
 }
 
-func Refund(tx MonzoTransaction, msg string) error {
+func ParseOrder(r *http.Request) (err error, tx Order) {
+	var data = MonzoWebHook{}
+
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&data)
+	if err != nil {
+		return errors.New("Failed to parse request body: " + err.Error()), tx
+	}
+
+	if data.Type != "transaction.created" {
+		return errors.New("Unexpected WebHook type: " + data.Type), tx
+	}
+
+	tx.SortCode = data.Data.CounterParty.Sort_Code
+	tx.AccountNumber = data.Data.CounterParty.Account_Number
+	tx.Amount = data.Data.Amount
+	tx.Currency = data.Data.Currency
+
+	if tx.SortCode == "" || tx.AccountNumber == "" {
+		return errors.New("Counterparty data missing"), tx
+	}
+
+	if data.Data.Amount != OrderAmountPence {
+		return errors.New(fmt.Sprintf("Wrong amount. Send %dp exactly", OrderAmountPence)), tx
+	}
+
+	if data.Data.Currency != "GBP" {
+		return errors.New("Wrong currency. Send GBP only"), tx
+	}
+
+	// TODO validate address
+
+	if !IsValidAddress(data.Data.Description) {
+		return errors.New("Reference field of transfer must contain a valid Ethereum address"), tx
+	}
+
+	tx.EthAddress = common.HexToAddress(data.Data.Description)
+
+	return nil, tx
+}
+
+func Refund(tx Order, msg string) error {
 	// TODO
+
+	if tx.SortCode == "" || tx.AccountNumber == "" || tx.Currency == "" {
+		log.Println("ERROR: An error occurred but we do not have enough information to issue a refund: " + msg)
+		return nil
+	}
+
+	log.Printf("TODO Refund %d (%s) to %s-%s. %s", tx.Amount, tx.Currency, tx.SortCode, tx.AccountNumber, msg)
 	return errors.New("Not implemented")
 }
 
-func BuyEther(amountPence uint) (err error, amountWei uint) {
+func BuyEtherOnCoinbase() (err error, amountWei uint) {
+	log.Printf("Buy %d worth of ETH on coinbase", EtherValuePence)
+
 	// TODO
-	return errors.New("Not implemented"), 0
+
+	return nil, 42
 }
 
 func SendEtherFromEtherDirectToUser(amountWei uint, to common.Address) error {
-	return errors.New("Not implemented")
+
+	log.Printf("Send %d wei from %s to %s", amountWei, AddressEtherDirect, to.Hex())
+
+	// TODO
+
+	return nil
+}
+
+func SendGbpFromMonzoToCoinbase() error {
+
+	log.Printf("Send %dp from Monzo to Coinbase", EtherValuePence)
+
+	// TODO
+
+	return nil
 }
 
 func SendEtherFromCoinbaseToEtherDirect(amountWei uint) error {
-	return errors.New("Not implemented")
+	log.Printf("Send %d wei from Coinbase to %s", amountWei, AddressEtherDirect)
+
+	// TODO
+
+	return nil
 }
 
 func HandleMonzoTransactionWebHook(w http.ResponseWriter, r *http.Request) {
 
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	// TODO verify that this is really from Monzo
 
+	log.Println("New order")
+
 	// Parse and validate the incoming bank transfer
-	err, tx := ParseMonzoTx(r)
+	err, order := ParseOrder(r)
+	log.Println(order)
 	if err != nil {
 		// If it's invalid refund the user
-		Refund(tx, err.Error())
+		Refund(order, err.Error())
 		return
 	}
 
 	// Try to buy ether
-	err, amountWei := BuyEther(tx.AmountPence - ServiceChargePence)
+	err, amountWei := BuyEtherOnCoinbase()
 	if err != nil {
 		// If buying ether fails, refund the user
-		Refund(tx, "Internal error")
+		Refund(order, "Internal error")
 		return
 	}
 
-	err = SendEtherFromEtherDirectToUser(amountWei, tx.EthAddress)
+	err = SendEtherFromEtherDirectToUser(amountWei, order.EthAddress)
 	if err != nil {
 		// If sending ether to user fails, refund
-		Refund(tx, "Internal error")
+		Refund(order, "Internal error")
 		// TODO try to sell the ether on coinbase?
 		return
+	}
+
+	err = SendGbpFromMonzoToCoinbase()
+	if err != nil {
+		log.Println(err)
 	}
 
 	err = SendEtherFromCoinbaseToEtherDirect(amountWei)
@@ -125,7 +234,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func init() {
 	for _, tmpl := range []string{"index"} {
-		filename := FileSystemRoot + tmpl + ".html"
+		filename := FileSystemRoot + "html/" + tmpl + ".html"
 		t, err := template.ParseFiles(filename)
 		if err != nil {
 			panic(err)
